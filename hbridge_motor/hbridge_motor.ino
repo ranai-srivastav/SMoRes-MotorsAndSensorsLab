4d13df6c8f64eceb615e5ce4bc0b3955e5ca0c16
ranais <ranaisrivastav@gmail.com>
2025-02-04 20:43:57 -0500

    write encoder here

--- hbridge_motor/hbridge_motor.ino
+++ hbridge_motor/hbridge_motor.ino
@@ -30,34 +30,31 @@ void loop() {
   analogWrite(DC_ENABLE, 255);
   digitalWrite(DC_L1, LOW);
   digitalWrite(DC_L2, HIGH);
-  if(t > 10000)
-  {
-    t = 0;
-    Ultrasound_DCMotor();
-  }
-  t = t+1;
-  // int val1 = digitalRead(DC_ENC1);
-  // int val2 = digitalRead(DC_ENC2);
 
-  // Serial.println("Val1:" + val1);
-  // Serial.println("Val2:" + val2);
+  int val1 = digitalRead(DC_ENC1);
+  int val2 = digitalRead(DC_ENC2);
 }
 
-void Ultrasound_DCMotor()
+void getCounts()
 {
-  // mV = mV/inch * inch + mV_bias ===> mV_bias = 0
-  // mV / (mV/inch) = inch
-  int US_ADC_counts = analogRead(US);
-  float US_voltage_mV = US_ADC_counts * 5.0 / 1023.0 * 1000;
-  float US_dist_inches = US_voltage_mV / 9.8;
-  Serial.print("US_voltage_mV: ");
-  Serial.print(US_voltage_mV);
-  Serial.print(" and US_dist_inches: ");
-  Serial.println(US_dist_inches);
+  
 }
 
-void read_sensor (){ 
-  int anVolt = analogRead(US); 
-  float mm = anVolt*5; //Takes bit count and converts it to mm 
-  float inches = mm/25.4; //Takes mm and converts it to inches 
-  } 
+// void Ultrasound_DCMotor()
+// {
+//   // mV = mV/inch * inch + mV_bias ===> mV_bias = 0
+//   // mV / (mV/inch) = inch
+//   int US_ADC_counts = analogRead(US);
+//   float US_voltage_mV = US_ADC_counts * 5.0 / 1023.0 * 1000;
+//   float US_dist_inches = US_voltage_mV / 9.8;
+//   Serial.print("US_voltage_mV: ");
+//   Serial.print(US_voltage_mV);
+//   Serial.print(" and US_dist_inches: ");
+//   Serial.println(US_dist_inches);
+// }
+
+// void read_sensor (){ 
+//   int anVolt = analogRead(US); 
+//   float mm = anVolt*5; //Takes bit count and converts it to mm 
+//   float inches = mm/25.4; //Takes mm and converts it to inches 
+//   } 
